f8d0da47e970113192775af72dd493eb9ab453d3
karenlstengel <#Justdance1>
2018-10-20 16:37:15 -0600

    mazes hardcoded

--- src/Main.py
+++ src/Main.py
@@ -4,3 +4,10 @@ import Maze
 
 if __name__ == "__main__":
     print("hello world")
+    print(Maze.maze5[2][2])
+    print(Maze.maze7[2][2])
+    print(Maze.maze8[2][2])
+    print(Maze.maze9[2][2])
+    print(Maze.maze10[2][2])
+    print(Maze.maze12[2][2])
+    print(Maze.maze14[2][2])
